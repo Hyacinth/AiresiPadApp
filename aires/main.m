e85e5abfa751f6ced6bc74e16cf127cf54a2d134
Gautham <gautham.vadivel@imaginea.com>
2013-04-04 17:35:17 +0530

    Initial Commit

---
+++ aires/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  aires
+//
+//  Created by Gautham on 04/04/13.
+//  Copyright (c) 2013 Imaginea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
